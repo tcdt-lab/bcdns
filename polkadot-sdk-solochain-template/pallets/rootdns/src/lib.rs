47e431a638041b387b3f18aad2e3fbb40b7428c0
khalidzahra <khalidzahra923@gmail.com>
2025-02-12 08:31:17 +0200

    Update chainspec storage documentation in pallet-rootdns

--- polkadot-sdk-solochain-template/pallets/rootdns/src/lib.rs
+++ polkadot-sdk-solochain-template/pallets/rootdns/src/lib.rs
@@ -1,7 +1,7 @@
 //! # Rootdns Pallet
 //!
 //! The Rootdns pallet allows users to register top-level domain (TLD) networks on the blockchain.
-//! The pallet stores the chain specification of the TLD network as a JSON file.
+//! The pallet stores the chain specification of the TLD network.
 //!
 //! ## Overview
 //! - Register TLDs with their chain specifications.
